d88a36bfa8088d6bcbd75ba15279a1e848450c57
n'da patrick ouffouet <ouffouetchrist@gmail.com>
2022-05-26 17:38:21 +0000

    delete help

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,3 @@ Route::get('/', function () {
 Route::get('/about-us', function () { 
     return view('pages/about');
 })->name('about');
-
-Route::get('/help', function(){
-    return view('pages/help');
-})->name('help');
